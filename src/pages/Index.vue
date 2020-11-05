2fae0187697c6fe9d917a8fe3e31c11f46c28c76
Yun <wyunfigo@hotmail.com>
2020-11-05 13:10:16 +0100

    toyindex fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,7 +6,7 @@
       :todos="todos"
       :meta="meta"
     ></example-component>
-    <q-btn label="To y-index" @click="toyindex()"/> 
+    <q-btn label="To y-index" @click="toyindex"/> 
   </q-page>
 </template>
 
@@ -14,15 +14,18 @@
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from '@vue/composition-api';
+import { METHODS } from 'http';
 
 export default defineComponent({
   name: 'PageIndex',
   components: { ExampleComponent },
   setup(props, {root}) {
+    
     function toyindex(): void {
-
       root.$router.push("y-index")
     }
+    
+    
     const todos = ref<Todo[]>([
       {
         id: 1,
@@ -48,7 +51,7 @@ export default defineComponent({
     const meta = ref<Meta>({
       totalCount: 1200
     });
-    return { todos, meta };
+    return { todos, meta, toyindex};
   }
 });
 </script>

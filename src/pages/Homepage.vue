c4a160de71f22181e4332d0be7c2f208544f7a2f
kikibubu05 <drosse_nadine@yahoo.de>
2020-11-09 14:33:49 +0100

    renamed and deleted pages

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,11 +1,5 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
     <q-btn rounded  color=primary label="next page" v-on:click="click"/>
     <q-btn rounded color=primary label="To y-index" @click="toyindex"/> 
   </q-page>
@@ -26,38 +20,9 @@ export default defineComponent({
     function click(): void {
       root.$router.push('DinsPage')
     }
-    
-    function toyindex(): void {
-      root.$router.push("y-index")
-    }
-    
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
+      
 
-    return { todos, meta, toyindex, click};
+    return {};
 
   }
 });

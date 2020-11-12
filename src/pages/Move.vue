f373f734779201732636bcad4247e6d6565e9a3d
kikibubu05 <drosse_nadine@yahoo.de>
2020-11-12 12:39:08 +0100

    move page style updated

--- src/pages/Move.vue
+++ src/pages/Move.vue
@@ -1,10 +1,8 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div class="buttons">
-      <q-btn class="runbtn" rounded icon="directions_run" size="50px"/>
-      <q-btn class="walkbtn" rounded icon="directions_walk" size="50px"/>
-      <q-btn class="bikebtn" rounded icon="directions_bike" size="50px"/>
-    </div>
+  <q-page class="maindiv">
+    <q-btn class="runbtn" rounded icon="directions_run" size="50px"/>
+    <q-btn class="walkbtn" rounded icon="directions_walk" size="50px"/>
+    <q-btn class="bikebtn" rounded icon="directions_bike" size="50px"/>
   </q-page>
 </template>
 
@@ -24,29 +22,33 @@ export default defineComponent({
 </script>
 
 <style>
+  .maindiv{
+    position: relative;
+    width: 90%; 
+    margin: auto;
+  }
+
   .runbtn{
     background-color: #E898A8;
     color: #E4032E;
-    height: 160px;
-    width: 350px;
-    display: block;
+    height: 20vh;
+    width: 100%;
+    margin-top: 12%;
   }
 
    .walkbtn{
     background-color: #8DB5DE;
     color: #004F9F;
-    height: 160px;
-    width: 350px;
-    display: block;
-    margin-top: 30px;
-    margin-bottom: 30px;
+    height: 20vh;
+    width: 100%;
+    margin-top: 12%;
   }
 
-.bikebtn{
+  .bikebtn{
     background-color: #85D1A2;
     color: #00983A;
-    height: 160px;
-    width: 350px;
-    display: block;
+    height: 20vh;
+    width: 100%;
+    margin-top: 12%;
   }
 </style>

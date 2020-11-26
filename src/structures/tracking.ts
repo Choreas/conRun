a64e51ca8b57a0405039c7c3e36ce3469a2b572e
Choreas <niklas.kosanke@web.de>
2020-11-26 16:41:45 +0100

    corrected datatype

--- src/structures/tracking.ts
+++ src/structures/tracking.ts
@@ -1,7 +1,7 @@
 export default [
   { name: "id",             type: "TEXT", id: true,  prim: true  },
   { name: "startTimestamp", type: "INTEGER", id: false,  prim: false  }, // unix timestamp, seconds since 1970
-  { name: "endTimestamp",   type: "TEXT", id: false, prim: false }, // unix timestamp, seconds since 1970
+  { name: "endTimestamp",   type: "INTEGER", id: false, prim: false }, // unix timestamp, seconds since 1970
   { name: "activity",       type: "TEXT", id: false, prim: false }, // 'walk' | 'run' | 'cycle'
   { name: "distance",       type: "REAL", id: false,  prim: false  }, // metres
 ]
